9d2e78d1d8463391791c00d69e5a76457ed42dfd
Joe Spencer <spencerjoe@mac.com>
2021-01-08 12:00:35 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline();
+
+tl.from('.content', {
+    y: '-30%',
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut
+})
+tl.from('.stagger1', {
+    opacity: 0,
+    y: -50,
+    stagger: .3,
+    duration: 2,
+    ease: Power4.easeOut
+}, "-=1.5")
+tl.from('.hero-design', {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    ease: Power4.easeOut
+},"-=2")
+
+
+gsap.from('.transition2', {
+    scrollTrigger: {
+        trigger: '.transition2',
+        start: "top bottom"
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    stagger: .3
+})
+
+gsap.from('.transition3', {
+    scrollTrigger: {
+        trigger: '.transition3',
+        start: "top bottom"
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    stagger: .6
+})
